61706ee3fb0028477521562bd17cf0888990bb63
Brian Icochea <ibrian93@gmail.com>
2023-12-24 12:30:20 +0100

    adding files cypress

---
+++ cypress/integration/startExplenationButton/startExplenationButton.js
@@ -0,0 +1,26 @@
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import LearningPage from '../common/LearningPage';
+
+Given('I\'m on the maximum page', () => {
+    LearningPage.visitMaximum();
+});
+
+Then('I see a start-explenation-button above the template', () => {
+    
+});
+
+Then('I see a start-explenation-button below the template', () => {
+   
+});
+
+When('I click on the start-explenation-button', () => {
+   
+});
+
+Then('the template is scrolled to the top of the browser', () => {
+   
+});
+
+Then('the explenation is started', () => {
+   
+});
